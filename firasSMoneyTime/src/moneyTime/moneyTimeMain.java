cd02e53ce1f3da22e8807e79cddb9e62ebeb8e7d
FIRAS ALTAYEB <tayebfiras@gmail.com>
2015-08-03 23:34:18 +0100

    part three of constructing the online mode created an answer options arraylist which contains all the question's answer options from the specified URL.

--- firasSMoneyTime/src/moneyTime/moneyTimeMain.java
+++ firasSMoneyTime/src/moneyTime/moneyTimeMain.java
@@ -20,7 +20,6 @@ public class MoneyTimeMain
 		ArrayList<String> UMData = new ArrayList<String>();
 		for(int i=0;i<807;i++)
 		{
-			//System.out.println(in.nextLine());
 			if(i<305)
 			{
 				in.nextLine();
@@ -36,7 +35,8 @@ public class MoneyTimeMain
 		ArrayList<String> MQuestions = new ArrayList<String>();
 		for(int i=0;i<=UMData.size();i+=10)
 		{
-			try{
+			try
+			{
 				MQuestions.add(UMData.get(i).substring(7,UMData.get(i).indexOf("?")+1));
 			}
 			catch(Exception e)
@@ -50,7 +50,7 @@ public class MoneyTimeMain
 		/*MCorreectAnswers stands for "modified CorreectAnswers", modified CorreectAnswers are the CorreectAnswers
 		  that do not have the "Answer: " String in their start */
 		ArrayList<String> MCorreectAnswers = new ArrayList<String>();
-		try{for(int i=7;i<=UMData.size();i+=10)
+		for(int i=7;i<=UMData.size();i+=10)
 		{
 			if(UMData.get(i).isEmpty())
 			{
@@ -61,13 +61,30 @@ public class MoneyTimeMain
 			{
 				MCorreectAnswers.add(" *"+UMData.get(i).substring(UMData.get(i).indexOf(' ')+1, UMData.get(i).length()));
 			}
-		}}
-		catch(Exception e){}
+		}
 		
+		//System.out.println(UMData.size());
+		/*MAnswersOptions stands for "modified answersOptions", modified answersOptions arrayList contains
+		  the answersOptions which are taken from the UNData arrayList */
+		ArrayList<String> MAnswersOptions = new ArrayList<String>();
+		for(int i=0;i<=UMData.size()-1;i++)
+		{
+			if(UMData.get(i).isEmpty())
+			{}
+			else if(UMData.get(i).charAt(1) == '*')
+			{
+				MAnswersOptions.add(UMData.get(i));
+			}
+			
+		}
 		
 		for(int i=0;i<MQuestions.size();i++)
 		{
 			System.out.println(i+" "+MQuestions.get(i));
+		}
+			for(int i=0;i<MAnswersOptions.size();i++)
+		{
+			System.out.println(i+" "+MAnswersOptions.get(i));
 		}
 		for(int i=0;i<MCorreectAnswers.size();i++)
 		{
