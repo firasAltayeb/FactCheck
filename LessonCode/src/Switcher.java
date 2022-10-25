2ade437af2cce0655c45de53a32611c231ce4f10
Firas Altayeb <tayebfiras@gmail.com>
2022-10-24 20:12:54 -0400

    updated switcher class code

--- LessonCode/src/Switcher.java
+++ LessonCode/src/Switcher.java
@@ -1,18 +1,46 @@
 public class Switcher {
     public static void main(String[] args) {
+        char letter = 'a';
+        String vowelsString = "aeiou";
 
-        char ch = 'z';
+        // if (vowelsString.contains(Character.toString(letter))) {
+        //     System.out.println(letter + " is vowel");
+        // } else {
+        //     System.out.println(letter + " is consonant");
+        // }
 
-        switch (ch) {
+        //String result = (vowelsString.contains("z")) ? "vowel" : "consonant";
+        System.out.println(vowelsString.contains(""+letter) ? "vowel" : "consonant");
+
+        // char[] vowelsArray = new char[5];
+        // vowelsArray[0] = 'a';
+        // vowelsArray[1] = 'e';
+        // vowelsArray[2] = 'i';
+        // vowelsArray[3] = 'o';
+        // vowelsArray[4] = 'u';
+
+        char[] vowelsArray = {'a', 'e', 'i', 'o', 'u'};
+        boolean isVowel = false;
+        for (char placeHolder : vowelsArray) {
+            if (letter == placeHolder) {
+                System.out.println("The letter " + letter + " is a vowel!");
+                isVowel = true;
+            }
+        }
+        if (!isVowel) {
+            System.out.println("The letter " + letter + " is not a vowel!");
+        }
+
+        switch (letter) {
             case 'a':
             case 'e':
             case 'i':
             case 'o':
             case 'u':
-                System.out.println(ch + " is vowel");
+                System.out.println(letter + " is vowel");
                 break;
             default:
-                System.out.println(ch + " is consonant");
+                System.out.println(letter + " is consonant");
         }
     }
 }
