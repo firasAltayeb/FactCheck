b3211fcab116de63fd79733ae4dbc7666f6226d9
Firas Altayeb <tayebfiras@gmail.com>
2022-11-08 21:06:00 -0500

    improved switcher lesson code

--- LessonCode/src/Switcher.java
+++ LessonCode/src/Switcher.java
@@ -1,49 +1,69 @@
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.TextStyle;
 import java.util.Locale;
 import java.util.Scanner;
+import java.time.*;
 
 public class Switcher {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your a character if vowel: ");
-        char letter = sc.nextLine().charAt(0);
+    // final indicates that the variable will not be altered, thus saving memory
+    final static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter a word to check if the first letter is a vowel or consonant:");
+        final char letter = sc.nextLine().charAt(0);
         if (Character.isLetter(letter))
+            // In switch, case is if and default is else
             switch (letter) {
                 case 'a':
                 case 'e':
                 case 'i':
                 case 'o':
                 case 'u':
-                    System.out.println(letter + " is vowel");
+                    System.out.println("switch: " + letter + " is a vowel!");
                     break;
                 default:
-                    System.out.println(letter + " is consonant");
+                    System.out.println("switch: " + letter + " is a consonant!");
             }
         else
-            System.out.println("The input " + letter + " is not a letter");
+            System.out.println("You have entered a number, not a word.");
 
         System.out.println("Enter your desired number to check if its even or odd: ");
-        var input =  sc.nextLine();
-        if (Character.isDigit(input.charAt(0))) {
-            final var value = Integer.parseInt(input) % 2.0;
-            if (value == 0.0) {
-            System.out.println("Input is even!");
-            } else if (value == 1.0) {
-                System.out.println("Input is odd!");
-            } else {
-                System.out.println("Input is odd, & input % 3 is equal to " + value % 3);
+        int num = 0;
+        boolean isNum = true;
+        final var input = sc.nextLine();
+        for (int i = 0; i < input.length(); i++)
+            if (!Character.isDigit(input.charAt(i))) {
+                System.out.println("You entered a sequence containing non-digits!");
+                isNum = false;
+                break;
             }
-        }  else
-            System.out.println("The input " + input + " is not a digit");
+        // if (Character.isDigit(input.charAt(0)))
+        if (isNum) {
+            num = Integer.parseInt(input);
+            switch (num % 2) {
+                case 0:
+                    System.out.println("Input is even!");
+                    break;
+                case 1:
+                    System.out.println("Input is odd!");
+                    break;
+                default:
+                    System.out.println("Input is odd and input % 2 = " + num % 2);
+                    break;
+            }
+        }
+
+        final double remFive = num % 5.0;
+        if (remFive == 1.0) {
+            System.out.println(input.charAt(0) + " % 5.0 = " + remFive);
+        }
 
         System.out.println("Enter your birth month to find its horoscope: ");
-        String birthMonth = sc.nextLine().toLowerCase();
-        Month currentMonth = LocalDate.now().getMonth();
-        String shortHand = currentMonth.getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
-        boolean isBirthDayMonth = birthMonth.equals(currentMonth.toString()) ||
+        final String birthMonth = sc.nextLine().toLowerCase();
+        final Month currentMonth = LocalDate.now().getMonth();
+        final String shortHand = currentMonth.getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
+        final String happyBirthDay = "Your birth day is in this month. Happy birthday!";
+        final boolean isBirthDayMonth = birthMonth.equals(currentMonth.toString()) ||
                 birthMonth.equals(currentMonth.getValue() + "") ||
                 birthMonth.equals(shortHand.toLowerCase());
         switch (birthMonth) {
@@ -52,24 +72,24 @@ public class Switcher {
             case "10":
                 System.out.println("October horoscope is Libra and Scorpio!");
                 if (isBirthDayMonth)
-                    System.out.println("Your birth day is in this month, happy birthday!");
+                    System.out.println(happyBirthDay);
                 break;
             case "november":
             case "nov":
             case "11":
                 System.out.println("November horoscope is Scorpio and Sagittarius!");
                 if (isBirthDayMonth)
-                    System.out.println("Your birth day is in this month, happy birthday!");
+                    System.out.println(happyBirthDay);
                 break;
             case "december":
             case "dec":
             case "12":
                 System.out.println("December horoscope is Sagittarius and Capricorn!");
                 if (isBirthDayMonth)
-                    System.out.println("Your birth day is in this month, happy birthday!");
+                    System.out.println(happyBirthDay);
                 break;
             default:
-                System.out.println("Your birth month is not in the system");
+                System.out.println("Your birth month is not in the system!");
         }
     }
 }
